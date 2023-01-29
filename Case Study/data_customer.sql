523493afb298322784e8a2100080e311ec8791af
Angga Chandra Putra <80445271+UltEng@users.noreply.github.com>
2023-01-30 03:08:29 +0800

    Add files via upload

--- Case Study/data_customer.sql
+++ Case Study/data_customer.sql
@@ -1,16 +1,40 @@
+with last_order as (
+select 
+	CustomerID,
+	CompanyName,
+	OrderDate as last_order_date
+from (select
+			a.CustomerID,
+			a.CompanyName,
+			b.OrderDate,
+			DENSE_RANK () over(PARTITION by a.CustomerID ORDER BY b.OrderDate DESC) as rnk
+		from Customers a
+		join Orders b
+		on a.CustomerID = b.CustomerID) subq
+where rnk = 1
+), 
+total_spend as(
 select 
 	a.CustomerID,
 	a.CompanyName,
-	b.OrderDate,
-	DATEPART(MONTH ,b.OrderDate) as bulan,
-	DATEPART(YEAR ,b.OrderDate) as tahun,
-	c.UnitPrice,
-	c.Quantity,
-	c.Discount,
-	c.UnitPrice * c.Quantity as sales_no_discount,
-	c.UnitPrice * c.Quantity * (1-c.Discount) as sales
+	COUNT(b.OrderID) as frequency,
+	SUM(c.UnitPrice * c.Quantity * (1-c.Discount)) as moneter
 from Customers a
-join Orders b
-on a.CustomerID = b.CustomerID 
-join [Order Details] c
+join Orders b 
+on a.CustomerID = b.CustomerID
+join [Order Details] c 
 on b.OrderID = c.OrderID 
+group BY 
+	a.CustomerID,
+	a.CompanyName
+)
+select 
+	lo.CustomerID,
+	lo.CompanyName,
+	lo.last_order_date,
+	DATEDIFF(DAY, lo.last_order_date, '1998-05-07') as recency,
+	ts.frequency,
+	ts.moneter
+from last_order lo
+join total_spend ts 
+on lo.CustomerID = ts.CustomerID
--- Case Study/data_produk.sql
+++ Case Study/data_produk.sql
@@ -2,10 +2,11 @@ SELECT
 	c.ProductID, 
 	c.ProductName,
 	d.CategoryName,
-	a.OrderDate,
 	DATEPART(MONTH ,a.OrderDate) as bulan,
 	DATEPART(YEAR ,a.OrderDate) as tahun,
-	b.UnitPrice * b.Quantity as sales
+	SUM(b.Quantity) as quantity,
+	SUM(b.UnitPrice * b.Quantity) as sales_no_discount,
+	SUM(b.UnitPrice * b.Quantity * (1-b.Discount)) as sales
 FROM Orders a
 JOIN [Order Details] b 
 ON a.OrderID = b.OrderID 
@@ -13,4 +14,10 @@ JOIN Products c
 ON b.ProductID = c.ProductID 
 JOIN Categories d
 ON c.CategoryID = d.CategoryID 
-ORDER BY c.ProductID 
+GROUP BY 	
+	c.ProductID, 
+	c.ProductName,
+	d.CategoryName,
+	DATEPART(MONTH ,a.OrderDate),
+	DATEPART(YEAR ,a.OrderDate)
+ORDER BY c.ProductID, DATEPART(YEAR ,a.OrderDate)
